961332bd2b91462e8dc8a976e2d041555897b102
shimulsust999 <shimulsust999@gmail.com>
2023-04-07 04:11:17 +0600

    showing total balance

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,12 +1,29 @@
+import { useAppSelector } from "@/redux/Store";
+import { ITransactionsType } from "@/redux/transaction/TransactionType";
 import React from "react";
 
 const Balance = () => {
+  const { transactions } = useAppSelector((state) => state.transaction);
+
+  const calculateIncome = (transactions: ITransactionsType[]) => {
+    let income = 0;
+    transactions.forEach((element: ITransactionsType) => {
+      const { type, amount } = element;
+      if (type === "income") income += amount as number;
+      else income -= amount as number;
+    });
+    return income;
+  };
   return (
     <div className="top_card">
       <p>Your Current Balance</p>
       <h3>
         <span>৳</span>
-        <span>10500</span>
+        {transactions.length > 0 ? (
+          <span>{calculateIncome(transactions)}</span>
+        ) : (
+          0
+        )}
       </h3>
     </div>
   );
